7d0108132e500118382fc9cc1a3b07f1d6f6bc42
Vidoje Zeljić <zeljicvidoje@gmail.com>
2023-12-13 23:06:39 +0100

    Create async.js

---
+++ async.js
@@ -0,0 +1,27 @@
+function test1() {
+    console.log("Test 1")
+    for (let i = 0; i < 10; i++) {
+        let p1 = new Promise((a, b) => {
+            console.log(`XXX 1.1: ${i}`)
+            a(i)
+            console.log(`XXX 1.2: ${i}`)
+        })
+
+        p1.then(e => console.log(`XXX 1: ${e}`))
+    }
+}
+
+function test2() {
+    console.log("Test 2")
+    async function asyncTest(i) {
+        console.log(`--- 2.1: ${i}`)
+        return i
+    }
+
+    for (let i = 0; i < 10; i++) {
+        asyncTest(i).then(e => console.log(`--- 2: ${e}`))
+    }
+}
+
+test1()
+test2()
